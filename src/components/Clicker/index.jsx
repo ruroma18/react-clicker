90939661d356c56035bd9c79d4554fb0f1b699b8
Roman Rudenko <ruroma18@gmail.com>
2022-05-04 12:40:34 +0300

    Add AutoClicker task solution

--- src/components/Clicker/index.jsx
+++ src/components/Clicker/index.jsx
@@ -1,37 +1,60 @@
-function Clicker({counter, step, handleStepChange, handleChangeMode, toggleChangeNumber}) {
-
+function Clicker({
+  counter,
+  step,
+  time,
+  workTime,
+  handleStepChange,
+  handleChangeMode,
+  toggleChangeNumber,
+  startAutoClick,
+  stopAutoClick,
+  handleTimeRange,
+}) {
   return (
     <article>
       <h1>Clicker</h1>
       <p>{counter}</p>
-        <label htmlFor="step">Change step: </label>
+      <label htmlFor="step">
+        Change step:
         <input
           id="step"
           type="number"
           value={step}
           onChange={handleStepChange}
         />
-        <p>Now step is: {step}</p>
-        <div>
-          {" "}
-          Change mode:
-          <input
-            type="radio"
-            value="Add"
-            name="mode"
-            onChange={handleChangeMode}
-            defaultChecked
-          />{" "}
-          Add
-          <input
-            type="radio"
-            value="Subtract"
-            name="mode"
-            onChange={handleChangeMode}
-          />{" "}
-          Subtract
-        </div>
-        <button onClick={toggleChangeNumber}>Change</button>
+      </label>
+      <p>Now step is: {step}</p>
+      <p>Auto Clicker work: {workTime} seconds</p>
+      <label htmlFor="step">
+        Change time in sec:
+        <input
+          id="time"
+          type="number"
+          value={time}
+          onChange={handleTimeRange}
+        />
+      </label>
+      <label>
+        Change mode:
+        <input
+          type="radio"
+          value="Add"
+          name="mode"
+          onChange={handleChangeMode}
+          defaultChecked
+        />
+        Add
+        <input
+          type="radio"
+          value="Subtract"
+          name="mode"
+          onChange={handleChangeMode}
+        />
+        Subtract
+      </label>
+      <button onClick={toggleChangeNumber}>Click</button>
+      <button onClick={startAutoClick}>Auto Click</button>
+      <button onClick={stopAutoClick}>Stop Auto Click</button>
     </article>
   );
 }
--- src/components/ClickerContainer/index.jsx
+++ src/components/ClickerContainer/index.jsx
@@ -9,7 +9,29 @@ class ClickerCounter extends React.Component {
       counter: 0,
       step: 1,
       mathOperator: "Add",
+      stepId: null,
+      time: 1,
+      workTime: 0,
+      workTimeId: null,
     };
+    this.isRendered = false;
+  }
+
+  componentDidMount() {
+    if (this.isRendered) {
+      return;
+    }
+    this.isRendered = true;
+    if (!this.state.stepId) {
+      this.startAutoClick();
+      setTimeout(this.stopAutoClick, 30000);
+    }
+  }
+
+  componentDidUpdate() {}
+
+  componentWillUnmount() {
+    this.stopAutoClick();
   }
 
   handleChangeMode = (e) => {
@@ -24,7 +46,13 @@ class ClickerCounter extends React.Component {
     });
   };
 
-  toggleChangeNumber = (e) => {
+  handleTimeRange = (e) => {
+    this.setState({
+      time: e.target.value,
+    });
+  };
+
+  toggleChangeNumber = () => {
     this.setState({
       counter:
         this.state.mathOperator === "Add"
@@ -33,16 +61,52 @@ class ClickerCounter extends React.Component {
     });
   };
 
+  countWorkTime = () => {
+    const workTimeId = setInterval(() => {
+      this.setState({
+        workTime: this.state.workTime + 1,
+        workTimeId,
+      });
+    }, 1000);
+  };
+
+  startAutoClick = () => {
+    if (!this.state.stepId) {
+      this.countWorkTime();
+      const stepId = setInterval(() => {
+        this.setState({
+          counter:
+            this.state.mathOperator === "Add"
+              ? this.state.counter + parseInt(this.state.step)
+              : this.state.counter - parseInt(this.state.step),
+          stepId,
+        });
+      }, parseInt(this.state.time) * 1000);
+    }
+  };
+
+  stopAutoClick = () => {
+    clearInterval(this.state.stepId);
+    clearInterval(this.state.workTimeId);
+    this.setState({ stepId: null });
+    this.setState({ workTimeId: null });
+  };
+
   render() {
-    const { counter, step } = this.state;
+    const { counter, step, time, workTime } = this.state;
 
     return (
-      <Clicker 
-      counter = {counter}
-      step = {step}
-      handleChangeMode = {this.handleChangeMode}
-      handleStepChange = {this.handleStepChange}
-      toggleChangeNumber = {this.toggleChangeNumber}
+      <Clicker
+        counter={counter}
+        step={step}
+        time={time}
+        workTime={workTime}
+        handleChangeMode={this.handleChangeMode}
+        handleStepChange={this.handleStepChange}
+        toggleChangeNumber={this.toggleChangeNumber}
+        startAutoClick={this.startAutoClick}
+        stopAutoClick={this.stopAutoClick}
+        handleTimeRange={this.handleTimeRange}
       />
     );
   }
